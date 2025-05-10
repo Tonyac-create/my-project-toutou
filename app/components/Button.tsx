486961e495e42b4c073909787c799d9bbad94f0d
Angelique <angelique.copere@gmail.com>
2025-05-10 14:42:43 +0200

    modify Button.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,13 +1,11 @@
 interface ButtonProps {
     children: React.ReactNode;
-    onClick?: () => void;
     className?: string;
 }
 
-export default function Button({ children, onClick, className = '' }: ButtonProps) {
+export default function Button({ children, className = '' }: ButtonProps) {
     return (
         <button
-            onClick={onClick}
             className={`bg-[var(--foreground)] text-[var(--foreground-black)] text-xl font-bold px-6 py-3 rounded-lg hover:bg-[var(--foreground-alt)] hover:text-[var(--background)] transition-colors ${className}`}
         >
             {children}
